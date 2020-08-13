37137821b4a37198232c65d198050b639e4e6979
Di-djeckson <didjeckson@gmail.com>
2020-08-13 13:49:13 +0300

    Сделал второе домашнее задание lesson02

--- script/script.js
+++ script/script.js
@@ -1,5 +1,23 @@
-let money, income, addExpenses, deposit, mission, period;
+let money = 43000,
+  income = "freelance",
+  addExpenses = "courses, skills, car",
+  deposit = true,
+  mission = 100000,
+  period = 12;
 
-alert(" первый alert ");
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log(" первый console.log(); ");
+console.log(addExpenses.length);
+
+console.log("Период равен " + period + " месяцев");
+
+console.log("Цель заработать " + mission + " гривен");
+
+addExpenses = addExpenses.toLowerCase();
+addExpenses = addExpenses.split(", ");
+console.log(addExpenses);
+
+let budgetDay = money / 30;
+console.log(budgetDay);
