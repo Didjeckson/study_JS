7ad0e23a40457509d7cabd62e5e015896036be59
Di-djeckson <didjeckson@gmail.com>
2020-08-15 04:59:52 +0300

    Сделано третье домашнее задание lesson03

--- script/script.js
+++ script/script.js
@@ -1,7 +1,15 @@
-let money = 43000,
+"use strict";
+
+let money = prompt("Ваш месячный доход?"),
   income = "freelance",
-  addExpenses = "courses, skills, car",
-  deposit = true,
+  addExpenses = prompt(
+    "Перечислите возможные расходы за рассчитываемый период через запятую"
+  ),
+  deposit = confirm("Есть ли у вас депозит в банке?"),
+  expenses1 = prompt("Введите обязательную статью расходов?"),
+  expenses2 = prompt("Введите обязательную статью расходов?"),
+  amount1 = prompt("Во сколько это обойдётся?"),
+  amount2 = prompt("Во сколько это обойдётся?"),
   mission = 100000,
   period = 12;
 
@@ -19,5 +27,21 @@ addExpenses = addExpenses.toLowerCase();
 addExpenses = addExpenses.split(", ");
 console.log(addExpenses);
 
-let budgetDay = money / 30;
-console.log(budgetDay);
+let budgetMonth = amount1;
+console.log("Бюджет на месяц " + budgetMonth);
+
+let aimPeriod = mission / (money - budgetMonth);
+console.log("Цель будет достигнута за: " + Math.ceil(aimPeriod) + " месяцев");
+
+let budgetDay = budgetMonth / 30;
+console.log("Бюджет на день: " + Math.floor(budgetDay));
+
+if (budgetDay >= 1200) {
+  console.log("У вас высокий уровень дохода");
+} else if (600 <= budgetDay < 1200) {
+  console.log("У вас средний уровень дохода");
+} else if (0 <= budgetDay < 600) {
+  console.log("К сожалению, у вас уровень дохода ниже среднего");
+} else if (budgetMonth < 0) {
+  console.log("Что-то пошло не так");
+}
